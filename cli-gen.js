a3be345ab2b0e86619943ac9df7f429bcbe460e7
Glitch (hello-express) <none>
2020-11-26 17:18:25 +0000

    🏨🎗 Checkpoint ./cli-gen.js:8045597/2543 ./package.json:8045597/1623

--- cli-gen.js
+++ cli-gen.js
@@ -1,5 +1,7 @@
 const { prompt } = require("enquirer");
+
 (async () => {
+  var validator = require('validator')
   const response = await prompt([
     {
       type: "input",
@@ -16,15 +18,24 @@ const { prompt } = require("enquirer");
     {
       type: "input",
       name: "image",
-      message: "Would you like to provide a image? You don't have to though."
-    },{
+      message: "Would you like to provide a image? You don't have to though.",validate(value, state, item, index) {
+        if(!validator.isURL(value)){
+          return false
+        } else {
+          return true
+        }
+      }
+    },
+    {
       type: "input",
       name: "author",
-      message: "Who are you? You can use a username or your name",{
+      message: "Who are you? You can use a username or your name",
+      required: true
+    },
+    {
       type: "input",
-      name: "description",
-      message: "Can you describe your website? (Description)"
-    }
+      name: "footer",
+      message: "Would you like to add a footer? Something like 'Copyright name, 2020'"
     }
   ]);
   console.log(response);
